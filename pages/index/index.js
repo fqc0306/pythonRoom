9e6a51109cc50db22ac018181f3f1ed0a0fde95a
fengqinchong <fengqinchong@xiaomi.com>
2020-03-12 18:46:13 +0800

    [Update]小程序添加表结构测试数据

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,7 +8,16 @@ Page({
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    fileData: []
+    fileData: [],
+    listData: [
+      { "code": "01", "text": "text1", "type": "type1" },
+      { "code": "02", "text": "text2", "type": "type2" },
+      { "code": "03", "text": "text3", "type": "type3" },
+      { "code": "04", "text": "text4", "type": "type4" },
+      { "code": "05", "text": "text5", "type": "type5" },
+      { "code": "06", "text": "text6", "type": "type6" },
+      { "code": "07", "text": "text7", "type": "type7" }
+    ]
   },
   //事件处理函数
   bindViewTap: function() {
@@ -25,7 +34,9 @@ Page({
       data: {}
     }).then(res => {
       that.setData({
-        fileData: res.data
+        
+        
+        fileData: res.result.split('\n')[1]
       })
       console.log('[downloadFile] success result：', res)
     }).catch(err => {
