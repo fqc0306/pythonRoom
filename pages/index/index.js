d10ebc38736a7cb84111733284104615c0242c52
fqc0306_windows <fengqinchong@126.com>
2020-03-15 13:18:52 +0800

    [Add]添加曲线图逻辑

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,7 @@
 //index.js
 //获取应用实例
+
+var wxCharts = require("../../utils/wxcharts.js");
 const app = getApp()
 
 Page({
@@ -8,11 +10,7 @@ Page({
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    listData: [
-      { code: "01", text: "text1", type: "type1" },
-      { code: "02", text: "text2", "type": "type2" },
-      { "code": "03", "text": "text3", "type": "type3" }
-    ]
+    listData: []
   },
   //事件处理函数
   bindViewTap: function() {
@@ -28,32 +26,91 @@ Page({
       name: 'downloadFile',
       data: {}
     }).then(res => {
-      var infos = res.result.split('\n')[1]
-      var jsonArry = {}
-      var i = 0
-      
-      for (i = 0; i < infos.split(',').length; i++) {
-        var codeKey = "code"
-        var code = infos.split(',')[0]
-        var textKey = "text"
-        var text = infos.split(',')[1]
-        var typeKey = "type"
-        var type = infos.split(',')[2]
-        var json = {
+      var j = 0
+      var rooms = res.result.split('\n')
+      var horizonData = []
+      console.log("rooms number:", rooms.length)
+      for (j = 0; j < rooms.length; j++) {
+
+        var infos = rooms[j]
+        var jsonArry = {}
+        var i = 0
+
+        for (i = 0; i < infos.split(',').length; i++) {
+          var codeKey = "code"
+          var code = infos.split(',')[0]
+          var textKey = "text"
+          var text = infos.split(',')[1]
+          var typeKey = "type"
+          var type = infos.split(',')[2]
+          var json = {}
+          jsonArry[codeKey] = code
+          jsonArry[textKey] = text
+          jsonArry[typeKey] = type
+          horizonData.push(code)
         }
-        jsonArry[codeKey] = code
-        jsonArry[textKey] = text
-        jsonArry[typeKey] = type
+
+        that.data.listData.push(jsonArry)
+        console.log('jsonArry:', jsonArry)
       }
-      that.data.listData.push(jsonArry)
-      console.log('jsonArry:', jsonArry)
       that.setData({
         listData: that.data.listData
       })
-      console.log('[downloadFile] success result：', res)
+
+      that.getMothElectro(horizonData)
+      console.log('[downloadFile] result：', res)
     }).catch(err => {
       console.log('[downloadFile] 失败：', err)
     })
+  },
+  getMothElectro: function(data) {
+
+    var windowWidth = 320;
+    try {
+      var res = wx.getSystemInfoSync();
+      windowWidth = res.windowWidth;
+    } catch (e) {
+      console.error('getSystemInfoSync failed!');
+    }
+    new wxCharts({ //当月用电折线图配置
+      canvasId: 'yueEle',
+      type: 'line',
+      categories: data, //categories X轴
+      animation: true,
+      background: '#f5f5f5',
+      series: [{
+        name: '总用电量',
+        //data: yuesimulationData.data,
+        data: [1, 6, 9, 1, 0],
+        format: function(val, name) {
+          return val.toFixed(2) + 'kWh';
+        }
+      }, {
+        name: '电池供电量',
+        data: [0, 6, 2, 2, 7],
+        format: function(val, name) {
+          return val.toFixed(2) + 'kWh';
+        }
+      }],
+      xAxis: {
+        disableGrid: true
+      },
+      yAxis: {
+        title: '当月用电(kWh)',
+        format: function(val) {
+          return val.toFixed(2);
+        },
+        max: 20,
+        min: 0
+      },
+      width: windowWidth,
+      height: 200,
+      dataLabel: false,
+      dataPointShape: true,
+      extra: {
+        lineStyle: 'curve'
+      }
+    });
   },
 
   onLoad: function() {
