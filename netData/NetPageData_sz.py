e48315e76fe1bc9f48085b56cf0d5d63b7075711
fengqinchong <fengqinchong@xiaomi.com>
2020-05-31 23:03:21 +0800

    [Update]解决beautifulsoup解析不全的问题 & 深圳房源还差价格详情页面

--- netData/NetPageData_sz.py
+++ netData/NetPageData_sz.py
@@ -8,29 +8,24 @@ import json
 import codecs
 import collections
 import time
+import requests
 
 def getSoupByUrl(url):
-
-	opener = urllib2.build_opener()
-	headers = {
-	  'User-Agent': 'Mozilla/5.0 (Windows NT 5.1; rv:10.0.1) Gecko/20100101 Firefox/10.0.1',
-	}
-	opener.addheaders = headers.items()
 	tryTimes = 0
 	maxTimes = 3
 	while (tryTimes < maxTimes):
 		try:
-			response = opener.open(url)
-			html = response.read().decode('utf-8')
-			soup = BeautifulSoup(html, "html.parser", from_encoding="utf-8")
-			tryTimes = maxTimes
+			response = requests.get(url)
+			soup = BeautifulSoup(response.text, "lxml") 
+			break
+
 		except Exception as e:
 			print('open url:'+url)
 			print(e)
 			tryTimes = tryTimes + 1
 
-
 	return soup
+
 
 
 def writeFile(dictObj, fileName, isAppend):
@@ -50,73 +45,75 @@ def writeFile(dictObj, fileName, isAppend):
 #whichBuild+房间号 为id: whickBuild:19#商品住宅; name:青年家园; whichProject:京房售证字(2020)10号
 def getAllRoom(name, whichBuild, url, whichProject, allRoomDict, allStatusDict):
 	soup = getSoupByUrl(url)
+	print(name)
+	print(whichBuild)
+	print(url)
+	print(whichProject)
 
-	tables = soup.find_all(id="table_Buileing")
-	alldivs = tables[0].find_all("div")
+	builds = soup.find_all(id='divShowBranch') #A座 B座 楼栋信息whichBuild_A座
+	aTags = builds[0].find_all("a")
+	for tag in aTags:
+		enterUrl = tag.get('href')
+		# if tag.get('style') == None:
+		# 	getAllRoom(name, whichBuild, enterUrl, whichProject, allRoomDict, allStatusDict)
+
+	tables = soup.find_all('table',{'class':'table ta-c table2'})
+	trs = tables[0].find_all("tr")
 	index = 0
 
-	for i in alldivs:
+	for tr in trs:
 		print ("------------------")
-		style = i.get("style").split(";")[1]##房间的售卖状态
-		link = i.find("a")
-		
-		roomDict = collections.OrderedDict()
-		statusDict = collections.OrderedDict()
-
-		index = index + 1
-		roomDict.update({'id' : index})
-		roomDict.update({'build' :  whichBuild.split("#")[0]})
-		roomDict.update({'project' : whichProject})
-
-		#link.text case1:2单元-801 case2:-034 case3:-1103a
-		if link.text.split("-").__len__() >= 2:
-			roomDict.update({'unit' : link.text.split("-")[0]})
-			roomDict.update({'room' : link.text.split("-")[1]})
-			if (link.text.split("-")[0]==''):
-				roomDict.update({'room' : link.text})
-		else:
-			roomDict.update({'unit' : ""})
-			roomDict.update({'room' : link.text})
-			print("error length, link text:" + link.text)
-
-		statusDict.update({'id' : index})
-		statusDict.update({'build' :  whichBuild.split("#")[0]})
-		statusDict.update({'project' : whichProject})
-		statusDict.update({'status' : style.split("#")[1]})
-
-		#link.text case1:2单元-801 case2:-034 case3:-1103a
-		if link.text.split("-").__len__() >= 2:
-			statusDict.update({'unit' : link.text.split("-")[0]})
-			statusDict.update({'room' : link.text.split("-")[1]})
-			if (link.text.split("-")[0]==''):
-				statusDict.update({'room' : link.text})
-		else:
-			statusDict.update({'unit' : ""})
-			statusDict.update({'room' : link.text})
-			print("error length, link text:" + link.text)
-
-		allStatusDict.append(statusDict)
-
-		print(link.text + ":" + link.get("href"))
-		href = "http://bjjs.zjw.beijing.gov.cn" + link.get("href")
-
-		soup = getSoupByUrl(href)
-		desc = soup.find_all(id='desc')
-
-		tempDict = collections.OrderedDict()
-		if desc != None and desc.__len__()/2 > 0:
-			for k in range(desc.__len__()/2):
-				key = desc[k*2].text.encode('utf-8').replace(' ', '').replace('　', '')
+		tds = tr.find_all('td')
+		for td in tds:
+			aTag = td.find('a')
+			if aTag != None and aTag.__len__() > 0:
+				style = aTag.text ##房间的售卖状态
+				divs = td.find_all('div')
 				
-				value = desc[k*2+1].text.split(" ")[0].encode('utf-8').replace(' ', '').replace('　', '')
-				tempDict.update({unicode(key, "utf-8") : unicode(value, "utf-8")})
+				roomDict = collections.OrderedDict()
+				statusDict = collections.OrderedDict()
 
-			roomDict.update({'detail' : tempDict})
-			allRoomDict.append(roomDict)
-		else:
-			print("length error:" + str(len(desc)) + ",url:" + url)
+				index = index + 1
+				roomDict.update({'id' : index})
+				roomDict.update({'build' :  whichBuild.split("#")[0]})
+				roomDict.update({'project' : whichProject})
 
-	print(alldivs.__len__())
+				#link.text case1:2单元-801 case2:-034 case3:-1103a
+				if divs.__len__() >= 0:
+					roomDict.update({'unit' : ""}) ##深圳没有单元号？
+					roomDict.update({'room' : divs[0].text})
+
+				statusDict.update({'id' : index})
+				statusDict.update({'build' :  whichBuild.split("#")[0]})
+				statusDict.update({'project' : whichProject})
+				statusDict.update({'status' : style})
+
+				if divs.__len__() >= 0:
+					statusDict.update({'unit' : ""}) ##深圳没有单元号？
+					statusDict.update({'room' : divs[0].text})
+
+				allStatusDict.append(statusDict)
+
+				# print(link.text + ":" + link.get("href"))
+				# href = "http://bjjs.zjw.beijing.gov.cn" + link.get("href")
+
+				# soup = getSoupByUrl(href)
+				# desc = soup.find_all(id='desc')
+
+				# tempDict = collections.OrderedDict()
+				# if desc != None and desc.__len__()/2 > 0:
+				# 	for k in range(desc.__len__()/2):
+				# 		key = desc[k*2].text.encode('utf-8').replace(' ', '').replace('　', '')
+						
+				# 		value = desc[k*2+1].text.split(" ")[0].encode('utf-8').replace(' ', '').replace('　', '')
+				# 		tempDict.update({unicode(key, "utf-8") : unicode(value, "utf-8")})
+
+				# 	roomDict.update({'detail' : tempDict})
+				allRoomDict.append(roomDict)
+				# else:
+				# 	print("length error:" + str(len(desc)) + ",url:" + url)
+
+	# print(alldivs.__len__())
 
 def getMoreBuildInfo(name, url, projectId, allStatusDict, allRoomDict, allItems):
 	soup = getSoupByUrl(url)
@@ -176,7 +173,6 @@ def getCertDetail(certUrl, detailUrl, projectId, name):
 	detailDict = collections.OrderedDict()
 	allItems = []
 	buileDict = collections.OrderedDict()
-	itemInfo = collections.OrderedDict()
 	titleInfo = []
 	index = 0
 
@@ -198,49 +194,64 @@ def getCertDetail(certUrl, detailUrl, projectId, name):
 			for i in range(tds.__len__()/2):	
 				infoKey = tds[2*i].text.replace(' ', '').replace('\r', '').replace('\n', '')
 				infoValue = tds[2*i+1].text.replace(' ', '').replace('\r', '').replace('\n', '')
-				print("" + infoKey + ": " + infoValue)
+				
 				if infoValue.find('--') == -1:
 					detailDict.update({infoKey : infoValue})
 
 	#项目详情
 	soup = getSoupByUrl(detailUrl)
-	table = soup.find_all('table',{'class':'table ta-c table2 table-white'})[0]
-	tableInfos = table.find_all('tr')
-	for j in range(tableInfos.__len__()):
-		tds = tableInfos[j].find_all('td')
+	tables = soup.find_all('table',{'class':'table ta-c table2 table-white'})
+
+	trs = tables[0].find_all('tr', recursive=False) #找到直接孩子
+	for j in range(trs.__len__()):
+		tds = trs[j].find_all('td')
 		if tds.__len__() > 1:
 			for i in range(tds.__len__()/2):	
 				infoKey = tds[2*i].text.replace(' ', '').replace('\r', '').replace('\n', '')
 				infoValue = tds[2*i+1].text.replace(' ', '').replace('\r', '').replace('\n', '')
-				print("" + infoKey + ": " + infoValue)
+				
 				if infoValue.find('--') == -1:
 					detailDict.update({infoKey : infoValue})
 
 	buileDict.update({'info': detailDict})
 	
-	buildTable = table.find_all('table',{'class':'table ta-c table2 table-white'})
+	buildTable = tables[0].find('table')
 
 	allRoomDict = []
 	allStatusDict = []
 
 	#获取楼盘所有楼栋的基本信息
 	
-	if (buildTable != None and buildTable.__len__()>0) :#没有有更多楼栋
-		trs = buildTable[0].find_all("tr")
+	if (buildTable != None) :#没有有更多楼栋
+
+		tableChild = tables[0].find_all('table',{'class':'table ta-c table2 table-white'})
+		
+		trs = tableChild[0].find_all('tr', recursive=False)
 		for tr in trs:
 			if tr.find_all('th').__len__() > 0:
 				ths = tr.find_all('th')
 				for it in ths:
 					titleInfo.append(it.text.replace(' ', '').replace('\r', '').replace('\n', ''))
+				continue
 			
 			if tr.find_all('td').__len__() > 0:
 				tds = tr.find_all('td')
 				index = 0
+
+				itemInfo = collections.OrderedDict()
 				for it in tds:
+					aTag = it.find_all("a")
+
 					itemInfo.update({titleInfo[index%titleInfo.__len__()] : it.text.replace(' ', '').replace('\r', '').replace('\n', '')})
 					index = index + 1
 
-		allItems.append(itemInfo)
+				if aTag != None and aTag.__len__() > 0:
+					href = aTag[0].get("href")
+					print('-----------href')
+					enterUrl = "http://zjj.sz.gov.cn/ris/bol/szfdc/" + href
+					print(enterUrl)
+					getAllRoom(name, itemInfo.get(list(itemInfo.keys())[1]), enterUrl, projectId, allRoomDict, allStatusDict)
+				allItems.append(itemInfo)
 			
 			# for value in values:
 
@@ -261,8 +272,8 @@ def getCertDetail(certUrl, detailUrl, projectId, name):
 			# 	itemInfo = collections.OrderedDict()
 			# index = index + 1
 
-	# writeFile(allStatusDict, name + "_" + projectId + "_dy.json", False)#写入所有栋楼的所有房间信息
-	# writeFile(allRoomDict, name + "_" + projectId + ".json", False)
+	writeFile(allStatusDict, name + "_" + projectId + "_dy.json", False)#写入所有栋楼的所有房间信息
+	writeFile(allRoomDict, name + "_" + projectId + ".json", False)
 
 	buileDict.update({'build_list': allItems})
 
@@ -276,7 +287,7 @@ def getCertDetail(certUrl, detailUrl, projectId, name):
 timeStr = str(long(time.time())/(24*60*60))
 indexTotal = 0
 startIndex = 0
-endIndex = 5
+endIndex = 1
 
 #地区内所有楼盘内容
 for page in range(0, 1):
@@ -289,7 +300,6 @@ for page in range(0, 1):
 	for index in range(0, infos.__len__()):
 
 		tags = infos[index].find_all("a")
-		print(tags)
 		if tags != None and tags.__len__() > 0:
 			url = "http://zjj.sz.gov.cn/ris/bol/szfdc/" + tags[0].get("href")
 			url = url.replace('./', '')
